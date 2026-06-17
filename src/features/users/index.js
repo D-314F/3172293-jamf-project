6a0e88f758155206eff84bd89d1e06713d315725
D-314F <fabianguzman206@gmail.com>
2026-06-16 21:25:45 -0500

    chore(users): add LoginForm export to barrel file

--- src/features/users/index.js
+++ src/features/users/index.js
@@ -1,4 +1,5 @@
 // src/features/users
 export { default as UserRegisterForm } from './components/UserRegisterForm';
+export { default as LoginForm } from "./components/LoginForm";
 // export { userSchema } from "./userSchema";
 // export { dishSchema } from "./dishSchema";
