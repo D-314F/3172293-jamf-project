1a6684f0a9f63cab2692a55beab7076e4c303cb9
maycolsilva2 <maykolestibensilva@gmail.com>
2026-08-11 19:01:16 -0500

    feat: se agrego el modulo de gestión de permisos

--- src/shared/layouts/Navbar.jsx
+++ src/shared/layouts/Navbar.jsx
@@ -6,7 +6,8 @@ import {
   UtensilsCrossed, 
   Truck, 
   Boxes, 
-  LogIn 
+  LogIn,
+  ShieldCheck,
 } from "lucide-react";
 import {
   IconButton,
@@ -135,6 +136,13 @@ export default function Navbar() {
                   <Link to="/dashboard/inventoryList" className="flex items-center gap-3 w-full py-1 text-white hover:text-amber-400 transition">
                     <Boxes size={18} className="text-amber-400" />
                     <span>Gestionar Inventario</span>
+                  </Link>
+                </DropdownItem>
+
+                <DropdownItem>
+                  <Link to="/dashboard/permissions" className="flex items-center gap-3 w-full py-1 text-white hover:text-amber-400 transition">
+                    <ShieldCheck size={18} className="text-amber-400" />
+                    <span>Gestionar Permisos</span>
                   </Link>
                 </DropdownItem>
 
