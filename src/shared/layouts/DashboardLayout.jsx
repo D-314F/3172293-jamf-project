d5154ef2d954728f35dabc91cc05293751ee3c7c
Jamesito-777 <jamesmachao992@gmail.com>
2026-09-10 18:50:33 -0500

    feat: Se agrego el footer con enlaces.

--- src/shared/layouts/DashboardLayout.jsx
+++ src/shared/layouts/DashboardLayout.jsx
@@ -1,54 +1,25 @@
 import { Outlet } from "react-router-dom";
 import authBg from "@/assets/images/bf-2.png";
-// import authBg2 from "@/assets/images/bf-3.png";// <-- esta es la imagen del login
 import { Navbar } from "@/shared";
-
-// import { UserRegisterForm } from "@/features/users";
-// import { LoginForm } from "@/features/users";
-
-// import DishForm from "@/features/dishes/components/dishForm";
-// import { ProviderCreateForm } from "@/features/providers";
-// import { OrderForm  } from "@/features/orders";
-// import { InventoryForm } from "@/features/inventory";
-// import { InventoryUltimosPasos } from "@/features/inventory";
-
+import Footer from "./Footer";
 
 export default function DashboardLayout() {
-    return (
-        <>
-        <div 
-        className = "min-h-screen w-full put-20"
-        style={{
-            backgroundImage: `url(${authBg})`, // <--- Esta manera es en la que colocamos imagenes
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-        }}
+  return (
+    <div
+      className="min-h-screen w-full flex flex-col"
+      style={{
+        backgroundImage: `url(${authBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Navbar />
 
-        // Este de aca es para mostrar la imagen el login
-        // style={{
-        //     backgroundImage: `url(${authBg2})`, // <--- Esta manera es en la que colocamos imagenes
-        //     backgroundSize: "cover",
-        //     backgroundPosition: "center",
-        // }}
-        >
-            <main className = "mx-auto">
+      <main className="flex-grow mx-auto">
+        <Outlet />
+      </main>
 
-                <Navbar/>
-
-                {/* <UserRegisterForm /> */}
-                {/* <LoginForm /> */}
-
-                {/* <ProviderCreateForm /> */}
-                {/* <OrderForm /> */}
-                {/* <InventoryForm /> */}
-                {/* <InventoryUltimosPasos /> */}
-                {/* <DishForm />   */}
-
-
-
-                <Outlet />
-            </main>
-        </div>
-        </>
-    );
+      <Footer /> 
+    </div>
+  );
 }
